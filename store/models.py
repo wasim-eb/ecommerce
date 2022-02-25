247375953ace6ba22aa9878164acaa8623635425
wasim-eb <Wasse.elb@gmail.com>
2022-02-25 23:46:03 +0000

    Changed from floatField to DecimalField when showing the price (inside the product model). Made migrations.

--- store/models.py
+++ store/models.py
@@ -19,7 +19,7 @@ def __str__(self):
 # is a digital product or a physical product that needs to be shipped
 class Product(models.Model):
 	name = models.CharField(max_length=200)
-	price = models.FloatField()
+	price = models.DecimalField(max_digits=7, decimal_places=2)
 	digital = models.BooleanField(default=False,null=True, blank=True)
 	image = models.ImageField(null=True, blank=True)
 
