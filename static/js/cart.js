69cbd86d4a694eb742c4bbc805505d051759dc59
wasim-eb <Wasse.elb@gmail.com>
2022-02-26 23:24:06 +0000

    Added a create function. When a user is not logged in and adds an item to their cart, the data will be stored in a cookie. Add & remove items from cookie function.

--- static/js/cart.js
+++ static/js/cart.js
@@ -7,14 +7,46 @@ for (i = 0; i < updateBtns.length; i++) {
 		console.log('productId:', productId, 'Action:', action)
 
 		console.log('USER:', user)
-		if (user === 'AnonymousUser'){
-			console.log('User is not logged in.')
+		if (user == 'AnonymousUser'){
+			addCookieItem(productId, action)
 			
 		}else{
 			updateUserOrder(productId, action)
 		}
 
 	})
+}
+
+//Add to cookie item function
+function addCookieItem(productId, action){
+	console.log('User is not logged in')
+
+	if (action == 'add'){
+		//Adds item if item is not already in the cart
+		//If the item is already in the cart, it adds
+		//to the quantity instead
+		if (cart[productId] == undefined){
+			cart[productId] = {'quantity':1}
+		}else{
+			cart[productId]['quantity'] += 1
+		}
+	}
+	//Decreases the quantity of the item by 1 if
+	//the item already exists in the cart.
+	//If the item quantity is 1, the item
+	//is removed completely from the cart
+	if (action == 'remove'){
+		cart[productId]['quantity'] -= 1
+
+		if (cart[productId]['quantity'] <= 0){
+			console.log('Remove Item')
+			delete cart[productId]
+		}
+	}
+
+	console.log('Cart:', cart)
+	document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+	location.reload()
 }
 
 
